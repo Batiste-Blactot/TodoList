cb844a9e996007114cf4333aa43ab39c809f80f2
Batiste-Blactot <batiste.blactot1@gmail.com>
2021-07-17 23:52:26 +0200

    redirection on login page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,7 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ * 
+ * @isGranted("IS_AUTHENTICATED_FULLY")
+ */
 class FrontController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
